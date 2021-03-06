cae4e4852b7fe1a73ac4241386275fdfd91a3ade
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2021-03-06 12:10:45 +0200

    to

--- src/main/java/org/obrii/mit/dp2021/Tools/Config.java
+++ src/main/java/org/obrii/mit/dp2021/Tools/Config.java
@@ -1,11 +1,9 @@
 package org.obrii.mit.dp2021.Tools;
 
 public class Config {
-<<<<<<< Updated upstream
-    private static String fileName="Data.txt";
-=======
+
     private static String fileName="";
->>>>>>> Stashed changes
+
 
     public static String getFileName(){
         return fileName;
