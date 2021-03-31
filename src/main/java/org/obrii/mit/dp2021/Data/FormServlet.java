3579b56ce5fef09b2f07694e1c1477d279d6f095
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2021-03-31 18:27:01 +0300

    lab4

--- src/main/java/org/obrii/mit/dp2021/Data/FormServlet.java
+++ src/main/java/org/obrii/mit/dp2021/Data/FormServlet.java
@@ -1,7 +1,6 @@
 package org.obrii.mit.dp2021.Data;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
